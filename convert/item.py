42bed56d141926c8b5ec43b735b3e171cbc29781
stevecassidy <none@none>
2013-08-05 13:30:12 +1000

    more output to errorlog

--- convert/item.py
+++ convert/item.py
@@ -16,6 +16,78 @@ from participant import participant_uri
 
 from participant import item_site_name
 
+def parse_media_filename(filename, errorlog=sys.stderr):
+     """Extract the channel name, media type and -n status from a filename
+     return a dictionary.
+ 
+
+ >>> im.parse_media_filename('1_178_1_2_150-ch1-maptask.wav')
+ {'basename': '1_178_1_2_150', 'version': 1, 'type': 'audio', 'channel': 'ch1-maptask'}
+     
+ >>> im.parse_media_filename('1_178_1_2_150-n-ch1-maptask.wav')
+ {'basename': '1_178_1_2_150', 'version': 2, 'type': 'audio', 'channel': 'ch1-maptask'}
+     
+ >>> im.parse_media_filename('1_178_1_2_150-n-n-ch1-maptask.wav')
+ {'basename': '1_178_1_2_150', 'version': 3, 'type': 'audio', 'channel': 'ch1-maptask'}
+ 
+ >>> im.parse_media_filename('1_178_1_2_150-n-n-n-n-n-ch1-maptask.wav')
+ {'basename': '1_178_1_2_150', 'version': 6, 'type': 'audio', 'channel': 'ch1-maptask'}
+ 
+ >>> im.parse_media_filename('1_178_1_2_150-camera-0-left.mp4')
+ {'basename': '1_178_1_2_150', 'version': 1, 'type': 'video', 'channel': 'camera-0-left'}
+ 
+ >>> im.parse_media_filename('1_178_2_16_001-camera-0-right.mp4')
+ {'basename': '1_178_2_16_001', 'version': 1, 'type': 'video', 'channel': 'camera-0-right'}
+ 
+ >>> im.parse_media_filename('1_1121_1_12_001-ch4-c2Left.wav')
+ {'basename': '1_1121_1_12_001', 'version': 1, 'type': 'audio', 'channel': 'ch4-c2Left'}
+ 
+ >>> im.parse_media_filename('1_1121_1_12_001-ch6-speaker-yes.wav')
+ {'basename': '1_1121_1_12_001', 'version': 1, 'type': 'audio', 'response': 'yes', 'channel': 'ch6-speaker'}
+ 
+ >>> im.parse_media_filename('1_178_1_2_150-camera-0-no-left.mp4')
+ {'basename': '1_178_1_2_150', 'version': 1, 'type': 'video', 'response': 'no', 'channel': 'camera-0-left'}
+ 
+ >>> im.parse_media_filename('1_178_1_2_150-n-n-camera-0-yes-left.mp4')
+ {'basename': '1_178_1_2_150', 'version': 3, 'type': 'video', 'response': 'yes', 'channel': 'camera-0-left'}
+ 
+ >>> im.parse_media_filename('1_178_1_2_150-ch6-speaker16.wav')
+ {'basename': '1_178_1_2_150', 'version': 1, 'type': 'audio', 'channel': 'ch6-speaker16'}
+     """
+     
+     
+     pattern_wav = "([0-9_]+)-((n-)*)(ch[0-9]-[a-zA-Z0-9]+)(-(yes|no))?\.wav"
+     
+     pattern_mp4 = "([0-9_]+)-((n-)*)((camera-[0-9])(-(yes|no))?(-left|-right))\.mp4"
+     
+     m_wav = re.match(pattern_wav, filename)
+     m_mp4 = re.match(pattern_mp4, filename)
+     
+     if m_wav:
+         (base, alln, n, channel, ignore, yesno) =  m_wav.groups() #@UnusedVariable
+         type = 'audio'
+     elif m_mp4:
+         (base, alln, n, ignore, camera, ignore, yesno, leftright ) = m_mp4.groups()
+         #print "MATCH: ", (base, alln, n, ignore, camera, ignore, yesno, leftright )
+         channel = camera + leftright
+         type = 'video'
+     else:
+         # unknown file pattern
+         errorlog.write("filename doesn't match media pattern: %s" % filename)
+         return dict()
+             
+     if n == None:
+         version = 1
+     else:
+         version = len(alln)/2 + 1
+     
+     result = {'basename': base, 'channel': channel, 'type': type, 'version': version}
+     if yesno != None:
+         result['response'] = yesno
+         
+     #print "PMF: ", filename, result
+     return result
+
 class ItemMapper:
     
     def __init__(self, server, errorlog=None):
@@ -31,7 +103,7 @@ class ItemMapper:
 
     def error(self, message):
         """Log an error"""
-        self.errorlog.write(message)
+        self.errorlog.write(message + "\n")
     
     
     def init_map(self):
@@ -100,7 +172,7 @@ class ItemMapper:
         
         # we modify the path based on the file type since we're splitting
         # audio and video data
-        minfo = parse_media_filename(filename)
+        minfo = parse_media_filename(filename, self.errorlog)
         path = os.path.join(minfo['type'], path)
         
         return DATA_NS[path]
@@ -149,12 +221,7 @@ class ItemMapper:
         
         return [(subj, NS.session, Literal(value))]
     
-    def item_participant(self, url):
-        """Return the participant Id (1_123) for this item"""
-    
-        md = read_metadata(url)
 
-        return "%(colour)s_%(animal)s" % md
         
     COMPONENTS = {'spontaneous': [
                         "interview",
@@ -211,7 +278,10 @@ class ItemMapper:
             graph.add((item_uri, AUSNC.interactivity, AUSNC.dialogue))
             graph.add((item_uri, AUSNC.speech_style, AUSNC.spontaneous))
         
-        
+
+
+ 
+
         
     
     def item_rdf(self, url, csvdata=None):
@@ -387,178 +457,117 @@ class ItemMapper:
         
         return graph
 
-
-def read_metadata(url):
-    """Read item metadata from the given url
-    return a dictionary of values
     
->>> mdfile = "test/1_1121_1_12_001.xml"
->>> read_metadata(mdfile)
-{'files': {'1_1121_1_12_001-ch6-speaker.wav': {'checksum': 'e0012015d6babdce61cb553939d87792', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch6-speaker.wav', 'version': 1, 'type': 'audio', 'channel': 'ch6-speaker'}, '1_1121_1_12_001-ch1-maptask.wav': {'checksum': '3a1ac90a5a3940ac1cb9046d5546b574', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch1-maptask.wav', 'version': 1, 'type': 'audio', 'channel': 'ch1-maptask'}, '1_1121_1_12_001-ch4-c2Left.wav': {'checksum': 'db028ab9647fe0e04377f338451ed53a', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch4-c2Left.wav', 'version': 1, 'type': 'audio', 'channel': 'ch4-c2Left'}, '1_1121_1_12_001-camera-0-left.mp4': {'checksum': '6065f4a33f4008b592a1f6d178bea5fb', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-camera-0-left.mp4', 'version': 1, 'type': 'video', 'channel': 'camera-0-left'}, '1_1121_1_12_001-ch5-c2Right.wav': {'checksum': '630d4d53a57e9f5ae01c6d764d8f169a', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch5-c2Right.wav', 'version': 1, 'type': 'audio', 'channel': 'ch5-c2Right'}}, 'participant': "Gold-Blainville's Beaked Whale", 'cameraSN1': '10251399', 'cameraSN0': '10251399', 'componentName': 'Words Session 1', 'colour': '1', 'component': '12', 'item': '1', 'session': '1', 'animal': '1121', 'timestamp': 'Mon Jul 18 16:48:43 2011', 'prompt': 'slide2.jpg', 'path': '/tmp/tmph7F7_g', 'basename': '1_1121_1_12_001'}
-
-# an example of regenerated metadata, has no basename so we need to reconstruct it
->>> mdfile = "test/1_178_4_12_001.xml"
->>> md = read_metadata(mdfile)
->>> md['basename']
-'1_178_4_12_001'
-
-# grab an example that has -n files
->>> mdurl = "test/1_178_2_16_001.xml"
->>> md = read_metadata(mdurl)
->>> md['cameraSN1']
-'11072159'
->>> md['files'].keys()
-['1_178_2_16_001-ch5-c2Right.wav', '1_178_2_16_001-n-ch6-speaker.wav', '1_178_2_16_001-ch3-strobe.wav', '1_178_2_16_001-n-ch1-maptask.wav', '1_178_2_16_001-ch4-c2Left.wav', '1_178_2_16_001-ch1-maptask.wav', '1_178_2_16_001-camera-0-left.mp4', '1_178_2_16_001-camera-0-right.mp4', '1_178_2_16_001-ch6-speaker.wav', '1_178_2_16_001-n-ch4-c2Left.wav', '1_178_2_16_001-ch2-boundary.wav', '1_178_2_16_001-n-ch5-c2Right.wav', '1_178_2_16_001-n-camera-0-right.mp4', '1_178_2_16_001-n-ch2-boundary.wav', '1_178_2_16_001-n-camera-0-left.mp4', '1_178_2_16_001-n-ch3-strobe.wav']
->>> md['files']['1_178_2_16_001-n-ch5-c2Right.wav']
-{'checksum': '2d5b13b05063a4b3b845672d1ca4eb91', 'basename': '1_178_2_16_001', 'filename': '1_178_2_16_001-n-ch5-c2Right.wav', 'version': 2, 'type': 'audio', 'channel': 'ch5-c2Right'}
-
-    """
-
-    result = dict()
+    def item_participant(self, url):
+        """Return the participant Id (1_123) for this item"""
     
-    # parse the xml, might fail but we'll just pass on the exception
-    # might have an http url but maybe a local file
-    if url.startswith('http'):
-        h = urllib2.urlopen(url)
-        xmltext = h.read()
-        h.close()
-    else:
-        h = open(url)
-        xmltext = h.read()
-        h.close()
+        md = self.read_metadata(url)
+
+        return "%(colour)s_%(animal)s" % md
+    
+    
+    def read_metadata(self, url):
+        """Read item metadata from the given url
+        return a dictionary of values
+    
+    >>> import sys
+    >>> sys.path.append("..")
+    >>> from ingest import SesameServer
+    >>> mdurl = "test/1_178_2_16_001.xml"
+    >>> serverurl = "http://sesame.stevecassidy.net/openrdf-sesame/repositories/bigasc"
+    >>> server = SesameServer(serverurl)
+    >>> im = ItemMapper(server)
+    >>> mdfile = "test/1_1121_1_12_001.xml"
+    >>> read_metadata(mdfile)
+    {'files': {'1_1121_1_12_001-ch6-speaker.wav': {'checksum': 'e0012015d6babdce61cb553939d87792', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch6-speaker.wav', 'version': 1, 'type': 'audio', 'channel': 'ch6-speaker'}, '1_1121_1_12_001-ch1-maptask.wav': {'checksum': '3a1ac90a5a3940ac1cb9046d5546b574', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch1-maptask.wav', 'version': 1, 'type': 'audio', 'channel': 'ch1-maptask'}, '1_1121_1_12_001-ch4-c2Left.wav': {'checksum': 'db028ab9647fe0e04377f338451ed53a', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch4-c2Left.wav', 'version': 1, 'type': 'audio', 'channel': 'ch4-c2Left'}, '1_1121_1_12_001-camera-0-left.mp4': {'checksum': '6065f4a33f4008b592a1f6d178bea5fb', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-camera-0-left.mp4', 'version': 1, 'type': 'video', 'channel': 'camera-0-left'}, '1_1121_1_12_001-ch5-c2Right.wav': {'checksum': '630d4d53a57e9f5ae01c6d764d8f169a', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch5-c2Right.wav', 'version': 1, 'type': 'audio', 'channel': 'ch5-c2Right'}}, 'participant': "Gold-Blainville's Beaked Whale", 'cameraSN1': '10251399', 'cameraSN0': '10251399', 'componentName': 'Words Session 1', 'colour': '1', 'component': '12', 'item': '1', 'session': '1', 'animal': '1121', 'timestamp': 'Mon Jul 18 16:48:43 2011', 'prompt': 'slide2.jpg', 'path': '/tmp/tmph7F7_g', 'basename': '1_1121_1_12_001'}
+    
+    # an example of regenerated metadata, has no basename so we need to reconstruct it
+    >>> mdfile = "test/1_178_4_12_001.xml"
+    >>> md = read_metadata(mdfile)
+    >>> md['basename']
+    '1_178_4_12_001'
+    
+    # grab an example that has -n files
+    >>> mdurl = "test/1_178_2_16_001.xml"
+    >>> md = read_metadata(mdurl)
+    >>> md['cameraSN1']
+    '11072159'
+    >>> md['files'].keys()
+    ['1_178_2_16_001-ch5-c2Right.wav', '1_178_2_16_001-n-ch6-speaker.wav', '1_178_2_16_001-ch3-strobe.wav', '1_178_2_16_001-n-ch1-maptask.wav', '1_178_2_16_001-ch4-c2Left.wav', '1_178_2_16_001-ch1-maptask.wav', '1_178_2_16_001-camera-0-left.mp4', '1_178_2_16_001-camera-0-right.mp4', '1_178_2_16_001-ch6-speaker.wav', '1_178_2_16_001-n-ch4-c2Left.wav', '1_178_2_16_001-ch2-boundary.wav', '1_178_2_16_001-n-ch5-c2Right.wav', '1_178_2_16_001-n-camera-0-right.mp4', '1_178_2_16_001-n-ch2-boundary.wav', '1_178_2_16_001-n-camera-0-left.mp4', '1_178_2_16_001-n-ch3-strobe.wav']
+    >>> md['files']['1_178_2_16_001-n-ch5-c2Right.wav']
+    {'checksum': '2d5b13b05063a4b3b845672d1ca4eb91', 'basename': '1_178_2_16_001', 'filename': '1_178_2_16_001-n-ch5-c2Right.wav', 'version': 2, 'type': 'audio', 'channel': 'ch5-c2Right'}
+    
+        """
+    
+        result = dict()
         
+        # parse the xml, might fail but we'll just pass on the exception
+        # might have an http url but maybe a local file
+        if url.startswith('http'):
+            h = urllib2.urlopen(url)
+            xmltext = h.read()
+            h.close()
+        else:
+            h = open(url)
+            xmltext = h.read()
+            h.close()
+            
+            
+        root = ElementTree.fromstring(xmltext)
         
-    root = ElementTree.fromstring(xmltext)
+        if root is None:
+            # no root element found  => corrupted xml
+            # we got nothing
+            return result
+     
+        result['files'] = dict()
+        
+        for node in root: 
+            name = node.tag
+            if name == "files":
+                # we make a list of the filenames
+                for ch in node.findall(".//file"):
+                    filename = ch.text
+                    # early versions were writing .xml files into the metadata, we don't
+                    # want to know about that
+                    if not filename.endswith(".xml"):
+                        
+                        # for each file we'll make a dictionary with it's properties
+                        if not result['files'].has_key(filename):
+                            result['files'][filename] = {'filename': filename}
+                        
+                        # we're ignoring the 'uploaded' attribute here
+                        # since it's not really of interest 
+                        
+                        if ch.get('md5hash') is not None:
+                            result['files'][filename]['checksum'] = ch.get('md5hash')
+                        # add new properties for file type and channel
+                        info = parse_media_filename(filename, self.errorlog)
+                        for key in info.keys():
+                            result['files'][filename][key] = info[key]
     
-    if root is None:
-        # no root element found  => corrupted xml
-        # we got nothing
-        return result
- 
-    result['files'] = dict()
     
-    for node in root: 
-        name = node.tag
-        if name == "files":
-            # we make a list of the filenames
-            for ch in node.findall(".//file"):
-                filename = ch.text
-                # early versions were writing .xml files into the metadata, we don't
-                # want to know about that
-                if not filename.endswith(".xml"):
-                    
-                    # for each file we'll make a dictionary with it's properties
+            elif name == "checksums":
+                # handle old style checksums in a separate element
+                for ch in node.findall(".//md5hash"):
+                    filename = ch.get('file')
                     if not result['files'].has_key(filename):
                         result['files'][filename] = {'filename': filename}
-                    
-                    # we're ignoring the 'uploaded' attribute here
-                    # since it's not really of interest 
-                    
-                    if ch.get('md5hash') is not None:
-                        result['files'][filename]['checksum'] = ch.get('md5hash')
-                    # add new properties for file type and channel
-                    info = parse_media_filename(filename)
-                    for key in info.keys():
-                        result['files'][filename][key] = info[key]
-
-
-        elif name == "checksums":
-            # handle old style checksums in a separate element
-            for ch in node.findall(".//md5hash"):
-                filename = ch.get('file')
-                if not result['files'].has_key(filename):
-                    result['files'][filename] = {'filename': filename}
-                result["files"][filename]['checksum'] = ch.text
-
-                    
-        elif name == "uploaded": 
-            pass
-        else:
-            result[name] = node.text
+                    result["files"][filename]['checksum'] = ch.text
     
-    # regenerated metadata has no basename
-    if not result.has_key('basename'):
-        result['item'] = int(result['item'])
-        result['basename'] = "%(colour)s_%(animal)s_%(session)s_%(component)s_%(item)03d" % result
-    
-    return result
-
-
-
-
-def parse_media_filename(filename):
-    """Extract the channel name, media type and -n status from a filename
-    return a dictionary.
-    
->>> parse_media_filename('1_178_1_2_150-ch1-maptask.wav')
-{'basename': '1_178_1_2_150', 'version': 1, 'type': 'audio', 'channel': 'ch1-maptask'}
-    
->>> parse_media_filename('1_178_1_2_150-n-ch1-maptask.wav')
-{'basename': '1_178_1_2_150', 'version': 2, 'type': 'audio', 'channel': 'ch1-maptask'}
-    
->>> parse_media_filename('1_178_1_2_150-n-n-ch1-maptask.wav')
-{'basename': '1_178_1_2_150', 'version': 3, 'type': 'audio', 'channel': 'ch1-maptask'}
-
->>> parse_media_filename('1_178_1_2_150-n-n-n-n-n-ch1-maptask.wav')
-{'basename': '1_178_1_2_150', 'version': 6, 'type': 'audio', 'channel': 'ch1-maptask'}
-
->>> parse_media_filename('1_178_1_2_150-camera-0-left.mp4')
-{'basename': '1_178_1_2_150', 'version': 1, 'type': 'video', 'channel': 'camera-0-left'}
-
->>> parse_media_filename('1_178_2_16_001-camera-0-right.mp4')
-{'basename': '1_178_2_16_001', 'version': 1, 'type': 'video', 'channel': 'camera-0-right'}
-
->>> parse_media_filename('1_1121_1_12_001-ch4-c2Left.wav')
-{'basename': '1_1121_1_12_001', 'version': 1, 'type': 'audio', 'channel': 'ch4-c2Left'}
-
->>> parse_media_filename('1_1121_1_12_001-ch6-speaker-yes.wav')
-{'basename': '1_1121_1_12_001', 'version': 1, 'type': 'audio', 'response': 'yes', 'channel': 'ch6-speaker'}
-
->>> parse_media_filename('1_178_1_2_150-camera-0-no-left.mp4')
-{'basename': '1_178_1_2_150', 'version': 1, 'type': 'video', 'response': 'no', 'channel': 'camera-0-left'}
-
->>> parse_media_filename('1_178_1_2_150-n-n-camera-0-yes-left.mp4')
-{'basename': '1_178_1_2_150', 'version': 3, 'type': 'video', 'response': 'yes', 'channel': 'camera-0-left'}
-
->>> parse_media_filename('1_178_1_2_150-ch6-speaker16.wav')
-{'basename': '1_178_1_2_150', 'version': 1, 'type': 'audio', 'channel': 'ch6-speaker16'}
-    """
-    
-    
-    pattern_wav = "([0-9_]+)-((n-)*)(ch[0-9]-[a-zA-Z0-9]+)(-(yes|no))?\.wav"
-    
-    pattern_mp4 = "([0-9_]+)-((n-)*)((camera-[0-9])(-(yes|no))?(-left|-right))\.mp4"
-    
-    m_wav = re.match(pattern_wav, filename)
-    m_mp4 = re.match(pattern_mp4, filename)
-    
-    if m_wav:
-        (base, alln, n, channel, ignore, yesno) =  m_wav.groups() #@UnusedVariable
-        type = 'audio'
-    elif m_mp4:
-        (base, alln, n, ignore, camera, ignore, yesno, leftright ) = m_mp4.groups()
-        #print "MATCH: ", (base, alln, n, ignore, camera, ignore, yesno, leftright )
-        channel = camera + leftright
-        type = 'video'
-    else:
-        # unknown file pattern
-        print "filename doesn't match media pattern: ", filename
-        return dict()
-            
-    if n == None:
-        version = 1
-    else:
-        version = len(alln)/2 + 1
-    
-    result = {'basename': base, 'channel': channel, 'type': type, 'version': version}
-    if yesno != None:
-        result['response'] = yesno
+                        
+            elif name == "uploaded": 
+                pass
+            else:
+                result[name] = node.text
         
-    #print "PMF: ", filename, result
-    return result
-
-
-
+        # regenerated metadata has no basename
+        if not result.has_key('basename'):
+            result['item'] = int(result['item'])
+            result['basename'] = "%(colour)s_%(animal)s_%(session)s_%(component)s_%(item)03d" % result
+        
+        return result
+    
 
 if __name__=='__main__':
         
