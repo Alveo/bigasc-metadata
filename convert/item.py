2de1be5df8027eb18a9c48f8608daf872e69c70c
stevecassidy <none@none>
2013-08-08 11:55:29 +1000

    Refactor file path code to new module. Add new dummy test data files so that item tests work again (now checking that files exist in metadata). Add test annotation data.

--- convert/item.py
+++ convert/item.py
@@ -15,108 +15,14 @@ from session import component_map
 from participant import participant_uri
 
 from participant import item_site_name
+from filepaths import parse_item_filename, parse_media_filename
 
-def parse_media_filename(filename, errorlog=sys.stderr):
-     """Extract the channel name, media type and -n status from a filename
-     return a dictionary.
- 
-
- >>> parse_media_filename('1_178_1_2_150-ch1-maptask.wav')
- {'basename': '1_178_1_2_150', 'version': 1, 'type': 'audio', 'channel': 'ch1-maptask'}
-     
- >>> parse_media_filename('1_178_1_2_150-n-ch1-maptask.wav')
- {'basename': '1_178_1_2_150', 'version': 2, 'type': 'audio', 'channel': 'ch1-maptask'}
-     
- >>> parse_media_filename('1_178_1_2_150-n-n-ch1-maptask.wav')
- {'basename': '1_178_1_2_150', 'version': 3, 'type': 'audio', 'channel': 'ch1-maptask'}
- 
- >>> parse_media_filename('1_178_1_2_150-n-n-n-n-n-ch1-maptask.wav')
- {'basename': '1_178_1_2_150', 'version': 6, 'type': 'audio', 'channel': 'ch1-maptask'}
- 
- >>> parse_media_filename('1_178_1_2_150-camera-0-left.mp4')
- {'basename': '1_178_1_2_150', 'version': 1, 'type': 'video', 'channel': 'camera-0-left'}
- 
- >>> parse_media_filename('1_178_2_16_001-camera-0-right.mp4')
- {'basename': '1_178_2_16_001', 'version': 1, 'type': 'video', 'channel': 'camera-0-right'}
- 
- >>> parse_media_filename('1_1121_1_12_001-ch4-c2Left.wav')
- {'basename': '1_1121_1_12_001', 'version': 1, 'type': 'audio', 'channel': 'ch4-c2Left'}
- 
- >>> parse_media_filename('1_1121_1_12_001-ch6-speaker-yes.wav')
- {'basename': '1_1121_1_12_001', 'version': 1, 'type': 'audio', 'response': 'yes', 'channel': 'ch6-speaker'}
- 
- >>> parse_media_filename('1_178_1_2_150-camera-0-no-left.mp4')
- {'basename': '1_178_1_2_150', 'version': 1, 'type': 'video', 'response': 'no', 'channel': 'camera-0-left'}
- 
- >>> parse_media_filename('1_178_1_2_150-n-n-camera-0-yes-left.mp4')
- {'basename': '1_178_1_2_150', 'version': 3, 'type': 'video', 'response': 'yes', 'channel': 'camera-0-left'}
- 
- >>> parse_media_filename('1_178_1_2_150-ch6-speaker16.wav')
- {'basename': '1_178_1_2_150', 'version': 1, 'type': 'audio', 'channel': 'ch6-speaker16'}
-     """
-     
-     
-     pattern_wav = "([0-9_]+)-((n-)*)(ch[0-9]-[a-zA-Z0-9]+)(-(yes|no))?\.wav"
-     
-     pattern_mp4 = "([0-9_]+)-((n-)*)((camera-[0-9])(-(yes|no))?(-left|-right))\.mp4"
-     
-     m_wav = re.match(pattern_wav, filename)
-     m_mp4 = re.match(pattern_mp4, filename)
-     
-     if m_wav:
-         (base, alln, n, channel, ignore, yesno) =  m_wav.groups() #@UnusedVariable
-         type = 'audio'
-     elif m_mp4:
-         (base, alln, n, ignore, camera, ignore, yesno, leftright ) = m_mp4.groups()
-         #print "MATCH: ", (base, alln, n, ignore, camera, ignore, yesno, leftright )
-         channel = camera + leftright
-         type = 'video'
-     else:
-         # unknown file pattern
-         errorlog.write("filename doesn't match media pattern: %s\n" % filename)
-         return dict()
-             
-     if n == None:
-         version = 1
-     else:
-         version = len(alln)/2 + 1
-     
-     result = {'basename': base, 'channel': channel, 'type': type, 'version': version}
-     if yesno != None:
-         result['response'] = yesno
-         
-     #print "PMF: ", filename, result
-     return result
- 
- 
- 
-def parse_item_filename(filename, errorlog=sys.stderr):
-    """Get the session, component and item ids
-    from the file name, return a dictionary with keys 'session', 'component', 'item'"""
-
-    import re
-    result = dict()
-    parse_it = re.compile(r'^(\d*)_(\d*)_(\d*)_(\d*)_(\d*)')
-    match = parse_it.search(filename)
-    if match:
-        groups = match.groups() 
-        result['colour'] = groups[0]
-        result['animal'] = groups[1]
-        result['speaker'] = groups[0]+"_"+groups[1]
-        result['session']   = groups[2]
-        result['component'] = groups[3]
-        result['item']      = str(int(groups[4]))  # do this to trim leading zeros
-    else:
-        errorlog.write("'%s' doesn't match the filename pattern\n" % filename )
-        
-    return result
 
 class ItemMapper:
     
-    def __init__(self, server, errorlog=None):
-        """Initialise an item mapper with a SesameServer instance"""
+    def __init__(self, errorlog=None):
+        """Initialise an item mapper"""
         
-        self.server = server
         if errorlog == None:
             self.errorlog = sys.stderr
         else:
@@ -298,10 +204,8 @@ class ItemMapper:
     >>> import sys
     >>> sys.path.append("..")
     >>> from ingest import SesameServer
-    >>> mdurl = "test/1_178_2_16_001.xml"
-    >>> serverurl = "http://sesame.stevecassidy.net/openrdf-sesame/repositories/bigasc"
-    >>> server = SesameServer(serverurl)
-    >>> im = ItemMapper(server)
+    >>> mdurl = "test/items/1_178_2_16_001.xml"
+    >>> im = ItemMapper()
     >>> graph = im.item_rdf(mdurl)
     >>> [t for t in graph.subject_objects(NS.cameraSN0)]
     [(rdflib.term.URIRef(u'http://id.austalk.edu.au/item/1_178_2_16_001'), rdflib.term.Literal(u'11051192'))]
@@ -312,13 +216,13 @@ class ItemMapper:
     # print graph.serialize(format='turtle') 
      
      # a maptask item with full metadata
-    >>> mfile = "test/1_530_3_8_001.xml"
+    >>> mfile = "test/items/1_530_3_8_001.xml"
     >>> graph = im.item_rdf(mfile)
     >>> [t for t in graph.subject_objects(NS.information_follower)]
     [(rdflib.term.URIRef(u'http://id.austalk.edu.au/item/1_530_3_8_001'), rdflib.term.URIRef(u'http://id.austalk.edu.au/participant/4_172'))]
                 
     # this one is a maptask item with no second speaker metadata
-    >>> mfile = "test/1_619_4_10_001.xml"
+    >>> mfile = "test/items/1_619_4_10_001.xml"
     >>> from ra_maptask import RAMapTask
     >>> mt = RAMapTask()
     >>> (s, m) = mt.read_all()
@@ -327,12 +231,12 @@ class ItemMapper:
     [(rdflib.term.URIRef(u'http://id.austalk.edu.au/item/1_619_4_10_001'), rdflib.term.URIRef(u'http://id.austalk.edu.au/participant/1_69'))]
     
     # this one is too, via a URL
-    >>> mdurl = "test/1_719_4_10_001.xml"
+    >>> mdurl = "test/items/1_719_4_10_001.xml"
     >>> graph3 = im.item_rdf(mdurl, m)
     >>> [t for t in graph3.subject_objects(NS.information_giver)]
     [(rdflib.term.URIRef(u'http://id.austalk.edu.au/item/1_719_4_10_001'), rdflib.term.URIRef(u'http://id.austalk.edu.au/participant/2_114'))]
     
-    >>> print graph3.serialize(format='turtle')   
+    # print graph3.serialize(format='turtle')   
 
     
         """
@@ -470,22 +374,20 @@ class ItemMapper:
     >>> import sys
     >>> sys.path.append("..")
     >>> from ingest import SesameServer
-    >>> mdurl = "test/1_178_2_16_001.xml"
-    >>> serverurl = "http://sesame.stevecassidy.net/openrdf-sesame/repositories/bigasc"
-    >>> server = SesameServer(serverurl)
-    >>> im = ItemMapper(server)
-    >>> mdfile = "test/1_1121_1_12_001.xml"
+    >>> mdurl = "test/items/1_178_2_16_001.xml"
+    >>> im = ItemMapper()
+    >>> mdfile = "test/items/1_1121_1_12_001.xml"
     >>> im.read_metadata(mdfile)
     {'files': {'1_1121_1_12_001-ch6-speaker.wav': {'checksum': 'e0012015d6babdce61cb553939d87792', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch6-speaker.wav', 'version': 1, 'type': 'audio', 'channel': 'ch6-speaker'}, '1_1121_1_12_001-ch1-maptask.wav': {'checksum': '3a1ac90a5a3940ac1cb9046d5546b574', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch1-maptask.wav', 'version': 1, 'type': 'audio', 'channel': 'ch1-maptask'}, '1_1121_1_12_001-ch4-c2Left.wav': {'checksum': 'db028ab9647fe0e04377f338451ed53a', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch4-c2Left.wav', 'version': 1, 'type': 'audio', 'channel': 'ch4-c2Left'}, '1_1121_1_12_001-camera-0-left.mp4': {'checksum': '6065f4a33f4008b592a1f6d178bea5fb', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-camera-0-left.mp4', 'version': 1, 'type': 'video', 'channel': 'camera-0-left'}, '1_1121_1_12_001-ch5-c2Right.wav': {'checksum': '630d4d53a57e9f5ae01c6d764d8f169a', 'basename': '1_1121_1_12_001', 'filename': '1_1121_1_12_001-ch5-c2Right.wav', 'version': 1, 'type': 'audio', 'channel': 'ch5-c2Right'}}, 'participant': "Gold-Blainville's Beaked Whale", 'cameraSN1': '10251399', 'cameraSN0': '10251399', 'componentName': 'Words Session 1', 'colour': '1', 'component': '12', 'item': '1', 'session': '1', 'animal': '1121', 'timestamp': 'Mon Jul 18 16:48:43 2011', 'prompt': 'slide2.jpg', 'path': '/tmp/tmph7F7_g', 'basename': '1_1121_1_12_001'}
     
     # an example of regenerated metadata, has no basename so we need to reconstruct it
-    >>> mdfile = "test/1_178_4_12_001.xml"
+    >>> mdfile = "test/items/1_178_4_12_001.xml"
     >>> md = im.read_metadata(mdfile)
     >>> md['basename']
     '1_178_4_12_001'
     
     # grab an example that has -n files
-    >>> mdurl = "test/1_178_2_16_001.xml"
+    >>> mdurl = "test/items/1_178_2_16_001.xml"
     >>> md = im.read_metadata(mdurl)
     >>> md['cameraSN1']
     '11072159'
