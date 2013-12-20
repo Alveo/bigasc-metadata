a979334293c2055d077af362c79c23e935ee7730
stevecassidy <none@none>
2013-12-20 17:35:09 +1100

    closing zip file too soon

--- zip_speaker.py
+++ zip_speaker.py
@@ -73,7 +73,7 @@ if __name__=='__main__':
                                             
                                             
                                                                                 
-        zfile.close()
+    zfile.close()
     
 
 
