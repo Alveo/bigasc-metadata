036e94f3420fa53aa2063e118271d0bdff045abf
stevecassidy <none@none>
2013-12-20 17:28:50 +1100

    zip_speaker takes a list of components to include in the zip file, also make including MAUS files optional

--- zip_speaker.py
+++ zip_speaker.py
@@ -17,14 +17,22 @@ if __name__=='__main__':
     
     import sys 
     
-    if len(sys.argv) != 2:
-        print "Usage: zip_speaker.py <spkrid>"
+    if len(sys.argv) < 2:
+        print "Usage: zip_speaker.py <spkrid> <comp> <comp> <comp>"
+        print "    where <comp> is a component name, if none give, then all components will be included"
         exit()
 
     datadir = configmanager.get_config('DATA_DIR')
     outdir =  configmanager.get_config('OUTPUT_DIR')
 
     spkrid = sys.argv[1]
+    if len(sys.argv) > 2:
+        components = sys.argv[2:]
+    else:
+        components = []
+        
+    # should be from the command line...
+    include_maus = False
     
     outfile = spkrid + ".zip"
     zfile = zipfile.ZipFile(outfile, 'w')
@@ -34,27 +42,38 @@ if __name__=='__main__':
     
     for site in os.listdir(mausdir):
         
-        sitedir = os.path.join(mausdir, site)
-        
+        sitedir = os.path.join(audiodir, site)
+        print sitedir
         if os.path.isdir(sitedir):
             for sdir in os.listdir(sitedir):
                 if sdir == spkrid:
                     speakerdir = os.path.join(sitedir, sdir)
+                    print speakerdir
                     for sessiondir in os.listdir(speakerdir):
+                        print sessiondir
                         for cdir in os.listdir(os.path.join(speakerdir, sessiondir)):
-                            compdir = os.path.join(speakerdir, sessiondir, cdir)
-                            for item in os.listdir(compdir):
-                                mauspath = os.path.join(site, sdir, sessiondir, cdir, item)
-                                mausfile = os.path.join(mausdir, mauspath)
-                                (base, ext) = os.path.splitext(item)
-                                audioname = base + ".wav"
-                                audiopath = os.path.join(site, sdir, sessiondir, cdir, audioname)
-                                audiofile = os.path.join(audiodir, audiopath)
-                                
-                                # add mausfile and audiofile to zip
-                                zfile.write(audiofile, audiopath)
-                                zfile.write(mausfile, mauspath)
-    zfile.close()
+                            print cdir
+                            if components != [] and cdir in components:
+                                compdir = os.path.join(speakerdir, sessiondir, cdir)
+                                for item in os.listdir(compdir):
+                                    
+                                    # add the audio file to the zip file
+                                    (base, ext) = os.path.splitext(item)
+                                    audioname = base + ".wav"
+                                    audiopath = os.path.join(site, sdir, sessiondir, cdir, audioname)
+                                    audiofile = os.path.join(audiodir, audiopath)
+                                    
+                                    zfile.write(audiofile, audiopath)
+                                    
+                                    if include_maus:
+                                        mauspath = os.path.join(site, sdir, sessiondir, cdir, item)
+                                        mausfile = os.path.join(mausdir, mauspath)
+                                        if sys.path.exists(mausfile):
+                                            zfile.write(mausfile, mauspath)
+                                            
+                                            
+                                                                                
+        zfile.close()
     
 
 
