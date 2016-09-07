cd026aa746a7e031dfcf395220e3f46eb5a77cb5
Steve Cassidy <steve.cassidy@mq.edu.au>
2016-09-07 19:53:37 +1000

    don't fall over for files we don't recognise

--- convert/filepaths.py
+++ convert/filepaths.py
@@ -1,7 +1,7 @@
 """
-  utilities for parsing item filenames and generating 
+  utilities for parsing item filenames and generating
   the right paths for storing data
-  
+
 """
 
 import sys
@@ -19,7 +19,7 @@ configmanager.configinit()
 
 def extension_mimetype(ext):
     """Return the mimetype for this extension"""
-    
+
     extmap = {'wav': 'audio/wav',
               'mp4': 'video/mp4',
               'raw16': 'video/raw16',
@@ -28,8 +28,8 @@ def extension_mimetype(ext):
     if extmap.has_key(ext):
         return extmap[ext]
     else:
-        raise Exception("Unknown extension: '%s'" % ext)
-    
+        # if we don't know, just return the extension
+        return ext
 
 
 def parse_media_filename(filename, errorlog=sys.stderr):
@@ -39,10 +39,10 @@ def parse_media_filename(filename, errorlog=sys.stderr):
 
 >>> parse_media_filename('1_178_1_2_150-ch1-maptask.wav')
 {'mimetype': 'audio/wav', 'basename': '1_178_1_2_150', 'version': 1, 'type': 'audio', 'channel': 'ch1-maptask'}
-    
+
 >>> parse_media_filename('1_178_1_2_150-n-ch1-maptask.wav')
 {'mimetype': 'audio/wav', 'basename': '1_178_1_2_150', 'version': 2, 'type': 'audio', 'channel': 'ch1-maptask'}
-    
+
 >>> parse_media_filename('1_178_1_2_150-n-n-ch1-maptask.wav')
 {'mimetype': 'audio/wav', 'basename': '1_178_1_2_150', 'version': 3, 'type': 'audio', 'channel': 'ch1-maptask'}
 
@@ -75,21 +75,21 @@ def parse_media_filename(filename, errorlog=sys.stderr):
 
 >>> parse_media_filename('1_178_1_2_150A-ch6-speaker16.wav')
 {'mimetype': 'audio/wav', 'sequence': 'A', 'basename': '1_178_1_2_150', 'version': 1, 'type': 'audio', 'channel': 'ch6-speaker16'}
-       
+
 >>> parse_media_filename('1_178_1_2_150-webvideo.mp4')
 {'mimetype': 'video/mp4', 'basename': '1_178_1_2_150', 'version': 1, 'type': 'video', 'channel': 'webvideo'}
 
               """
-    
+
     basename = os.path.basename(filename)
-    
+
     pattern_general = "([0-9_]+)([A-Z]?)-((n-)*)(ch[0-9]-[a-zA-Z0-9]+)(-(yes|no))?\.(.*)"
-    
+
     pattern_video = "([0-9_]+)([A-Z]?)-((n-)*)((camera-[0-9])(-(yes|no))?(-left|-right)|webvideo)\.(.*)"
-    
+
     m_gen = re.match(pattern_general, basename)
     m_vid = re.match(pattern_video, basename)
-    
+
     if m_gen:
         (base, ab, alln, n, channel, ignore, yesno, ext) =  m_gen.groups() #@UnusedVariable
         #print "MATCH: ", (base, ab, alln, n, channel, ignore, yesno, ext )
@@ -99,7 +99,7 @@ def parse_media_filename(filename, errorlog=sys.stderr):
             tipe = ext
     elif m_vid:
         (base, ab, alln, n, ignore, camera, ignore, yesno, leftright, ext) = m_vid.groups()
-        
+
         if camera != None:
             channel = camera + leftright
         else:
@@ -112,7 +112,7 @@ def parse_media_filename(filename, errorlog=sys.stderr):
         # unknown file pattern
         errorlog.write("filename doesn't match media pattern: %s\n" % basename)
         return dict()
-    
+
     mimetype = extension_mimetype(ext)
 
     sequence = None
@@ -125,41 +125,41 @@ def parse_media_filename(filename, errorlog=sys.stderr):
             version = 1
     else:
         version = len(alln)/2 + 1
-    
 
-    result = {'basename': base, 
-              'channel': channel, 
-              'type': tipe, 
+
+    result = {'basename': base,
+              'channel': channel,
+              'type': tipe,
               'mimetype': mimetype,
               'version': version}
-    
+
     if sequence != None:
         result['sequence'] = sequence
-        
+
     if yesno != None:
         result['response'] = yesno
-        
+
     #print "PMF: ", filename, result
     return result
- 
+
 def parse_item_filename(filename, errorlog=sys.stderr):
     """Get the session, component and item ids
     from the file name, return a dictionary with keys 'session', 'component', 'item'
-    
+
     >>> parse_item_filename('1_178_1_2_150.xml')
     {'basename': '1_178_1_2_150', 'component': '2', 'item': '150', 'session': '1', 'speaker': '1_178', 'animal': '178', 'colour': '1'}
     >>> parse_item_filename('../test/University_of_Tasmania,_Hobart/Spkr2_2/Spkr2_2_Session1/Session1_11/2_2_1_11_003')
     {'basename': '2_2_1_11_003', 'component': '11', 'item': '3', 'session': '1', 'speaker': '2_2', 'animal': '2', 'colour': '2'}
 
-    
+
     """
-    
+
     basename = os.path.basename(filename)
     result = dict()
     parse_it = re.compile(r'^((\d*)_(\d*)_(\d*)_(\d*)_(\d*))')
     match = parse_it.search(basename)
     if match:
-        groups = match.groups() 
+        groups = match.groups()
         result['basename'] = groups[0]
         result['colour'] = groups[1]
         result['animal'] = groups[2]
@@ -169,26 +169,26 @@ def parse_item_filename(filename, errorlog=sys.stderr):
         result['item']      = str(int(groups[5]))  # do this to trim leading zeros
     else:
         errorlog.write("'%s' doesn't match the filename pattern\n" % filename )
-        
+
     return result
 
 def item_file_uri(filename, dirname=None):
     """Given a filename for one of the files in an item, return a URI for
      this file on the data server
-     
+
 >>> item_file_uri('1_178_1_2_150-ch6-speaker16.wav')
 rdflib.term.URIRef(u'http://data.austalk.edu.au/audio/ANU/1_178/1/words-1/1_178_1_2_150-ch6-speaker16.wav')
 
      """
     path = item_file_path(filename, dirname)
-    
+
     return DATA_NS[path]
 
 def item_file_path(filename, dirname=None):
-    """Given a filename for one of the files in an item, return a path that we can 
+    """Given a filename for one of the files in an item, return a path that we can
     use to store it in the right place. This is a relative path, you still
     need to add OUTPUT_DIR to get an absolute path.
-    
+
 >>> item_file_path('1_178_1_2_150-ch6-speaker16.wav')
 'audio/ANU/1_178/1/words-1/1_178_1_2_150-ch6-speaker16.wav'
 >>> item_file_path('1_178_1_2_150A-ch6-speaker16.wav')
@@ -202,15 +202,15 @@ def item_file_path(filename, dirname=None):
 
     info = parse_item_filename(filename)
     info['filename'] = os.path.basename(filename)
-            
+
     m = component_map()
     info['componentName'] = m[int(info['component'])]
     info['site'] = item_site_name(participant_uri(info['colour'], info['animal']))
-    
+
     path = DATA_URI_TEMPLATE % info
-    
+
     if dirname == None:
-        
+
         # we modify the path based on the file type since we're splitting
         # audio and video data
         minfo = parse_media_filename(filename)
@@ -219,15 +219,15 @@ def item_file_path(filename, dirname=None):
         else:
             dirname = ''
 #            raise Exception("Can't work out directory name in item_file_path(%s)" % filename)
-        
+
     path = os.path.join(dirname, path)
-    
+
     return path
 
 def item_file_basename(filename):
     """Given a filename for one of the files in an item, return the basename
     of the item
-    
+
 >>> item_file_basename('1_178_1_2_150-ch6-speaker16.wav')
 '1_178_1_2_150'
 >>> item_file_basename('2_267_1_2_001-ch6-speaker.TextGrid')
@@ -236,23 +236,23 @@ def item_file_basename(filename):
 '2_2_1_11_003'
 
     """
-    
+
     info = parse_item_filename(filename)
     return info['basename']
 
 def versionselect():
     """Load versionselect data from the website (or a chached
     copy) and return the dictionary"""
-    
+
     global VERSIONS
-    
+
     try:
         return VERSIONS
     except:
         # VERSIONS not bound
         url = configmanager.get_config("VERSIONSELECT_URL")
         cache = configmanager.get_config("VERSIONSELECT_CACHE", "versionselect.json")
-        
+
         if os.path.exists(cache):
             h = open(cache)
             json_text = h.read()
@@ -265,7 +265,7 @@ def versionselect():
             h = open(cache, 'w')
             h.write(json_text)
             h.close()
-        
+
         VERSIONS = json.loads(json_text)
         return VERSIONS
 
@@ -274,17 +274,17 @@ def item_file_versions(path):
     the good and bad versions are.  The data comes from
     the versionselect application where there are more
     than one version of a recording for an item.
-    
+
     Return
-    
+
     {'rejected': [<list of rejected files>],
      'good': {'1_1216_1_4_001A': ['path/to/1_1216_1_4_001-n-camera-0-left.mp4', 'path/to/1_1216_1_4_001-n-camera-0-right.mp4'],
               '1_1216_1_4_001B': etc.,
               }
     }
-    
+
     if there is no version info then add the key 'versioninfo' with the value 'missing'
-    
+
 >>> x = item_file_versions('test/University_of_the_Sunshine_Coast,_Maroochydore/Spkr1_1216/Spkr1_1216_Session1/Session1_11/1_1216_1_11_016')
 >>> x['rejected']
 []
@@ -308,12 +308,12 @@ def item_file_versions(path):
 >>> len(x['good']['1_1216_1_4_001A'])
 8
     """
-    
+
     versions = versionselect()
-    
+
     basename = item_file_basename(path)
     all_files = item_files(path)
-    
+
     # do we have more than one version
     fv = dict()
     for fn in all_files:
@@ -327,22 +327,22 @@ def item_file_versions(path):
         else:
             # filename is malformed - message already printed by parse_media_filename
             pass
-    
+
     if len(fv) <= 1:
         # we only have one version (or zero if there is only an xml file for this item)
         return {'rejected': [], 'good': {basename: all_files}}
-    
-    # see if we have versionselect data for this item    
+
+    # see if we have versionselect data for this item
     if versions.has_key(basename):
         goodversions = versions[basename]
         goodfiles = dict()
         badfiles = []
-        
+
         # a zero in goodversions means there's no good version
         if 0 in goodversions:
             print "No good version of ", basename
             return {'rejected': all_files, 'good': {}}
-        
+
         N = 0
         for v in goodversions:
             if fv.has_key(v):
@@ -351,7 +351,7 @@ def item_file_versions(path):
                 goodfiles[newbase] = files
                 del fv[v]
                 N += 1
-            
+
         # now badfiles is the rest
         for v in fv.keys():
             badfiles.extend(fv[v])
@@ -361,15 +361,15 @@ def item_file_versions(path):
     else:
         print "Duplicate recordings and no version info for ", basename
         return {'rejected': [], 'good': {}, 'versioninfo': 'missing'}
-    
-    
-    
+
+
+
 def generate_new_item_basename(basename, n, total):
-    """Generate a basename for an item that might be 
+    """Generate a basename for an item that might be
     one of a number of good versions. n is the index
     of this version, total is the total number of good
     versions
-    
+
 >>> generate_new_item_basename('1_123_1_2_001', 0, 1)
 '1_123_1_2_001'
 >>> generate_new_item_basename('1_123_1_2_001', 0, 2)
@@ -403,11 +403,11 @@ def change_item_file_basename(f, basename):
         return basename+rest
     else:
         raise Exception("filename '%s' doesn't match pattern in change_item_file_basename" % f)
-    
-    
-    
-    
-        
+
+
+
+
+
 def item_files(path):
     """Given the base path of an item, return a list
     of the files associated with that item - ie. all
@@ -417,9 +417,9 @@ def item_files(path):
 >>> sorted(item_files('University_of_the_Sunshine_Coast,_Maroochydore/Spkr1_1216/Spkr1_1216_Session1/Session1_11/1_1216_1_11_003'))
 []
     """
-    
+
     from glob import glob
-    
+
     files = glob(path + '*')
     return [f for f in files if not f.endswith('.xml')]
 
@@ -430,12 +430,6 @@ def item_files(path):
 ## generate a component map, do this only once and we'll use it below
 
 if __name__=='__main__':
-        
+
     import doctest
     doctest.testmod()
-
-
-    
-        
-        
-
